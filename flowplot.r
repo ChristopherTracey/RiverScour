643ec46e72d69958d05173a17085ed51e493b5c1
Christoper Tracey <ctracey@paconserve.org>
2019-09-16 11:51:24 -0400

    change from yough to flowdata

--- flowplot.r
+++ flowplot.r
@@ -10,68 +10,68 @@ pCode <- "00060"
 start.date <- "2002-10-01"
 end.date <- "2019-06-30"
 
-yough <- readNWISuv(siteNumbers = siteNo,
+FlowData <- readNWISuv(siteNumbers = siteNo,
                      parameterCd = pCode,
                      startDate = start.date,
                      endDate = end.date)
 
-names(yough)
+names(FlowData)
 
-yough <- renameNWISColumns(yough)
+FlowData <- renameNWISColumns(FlowData)
 
-head(yough)
+head(FlowData)
 
 
-yough$year <- year(yough$dateTime)
-yough$month <- month(yough$dateTime)
-yough$day <- day(yough$dateTime)
-yough$doy <- yday(yough$dateTime)
+FlowData$year <- year(FlowData$dateTime)
+FlowData$month <- month(FlowData$dateTime)
+FlowData$day <- day(FlowData$dateTime)
+FlowData$doy <- yday(FlowData$dateTime)
 
-yough$flowsmo <- na.approx(yough$Flow_Inst)
-yough$flowlog <- log(yough$flowsmo)
+FlowData$flowsmo <- na.approx(FlowData$Flow_Inst)
+FlowData$flowlog <- log(FlowData$flowsmo)
 
 library(reshape2)
-# yough1 <- yough[c("year","doy","Flow_Inst")]
-yough1 <- yough[c("year","doy","flowlog")]
-yough1 <- acast(yough1, year~doy, fun.aggregate=mean, margins = TRUE)
+# FlowData1 <- FlowData[c("year","doy","Flow_Inst")]
+FlowData1 <- FlowData[c("year","doy","flowlog")]
+FlowData1 <- acast(FlowData1, year~doy, fun.aggregate=mean, margins = TRUE)
 
-# yough1[is.nan(yough1)] <- 0
-#yough1[is.nan(yough1)] = 0
-#plot_ly(data=yough, x=~doy ,y=~year, z=~Flow_Inst, type="surface")
-#yough1s <- matrix(smooth(yough1), nrow=16, ncol=367)
+# FlowData1[is.nan(FlowData1)] <- 0
+#FlowData1[is.nan(FlowData1)] = 0
+#plot_ly(data=FlowData, x=~doy ,y=~year, z=~Flow_Inst, type="surface")
+#FlowData1s <- matrix(smooth(FlowData1), nrow=16, ncol=367)
 # library(oce)
-# yough1s <- matrixSmooth(yough1)
+# FlowData1s <- matrixSmooth(FlowData1)
 
-plot_ly(z=yough1, type="surface")
+plot_ly(z=FlowData1, type="surface")
 
 #mean by day of year
-yough$period <- ifelse(yough$year>2010, "recent","baseline")  
+FlowData$period <- ifelse(FlowData$year>2010, "recent","baseline")  
 
 library(dplyr)
 se <- function(x) sqrt(var(x)/length(x))
-yough_meandaily <- yough %>% group_by(doy, period) %>% summarise_at(vars(Flow_Inst), funs(mean, max, sd, se))
+FlowData_meandaily <- FlowData %>% group_by(doy, period) %>% summarise_at(vars(Flow_Inst), funs(mean, max, sd, se))
 
-plot(yough_meandaily$mean, type="l", col="blue")
-arrows(yough_meandaily$doy,yough_meandaily$mean, yough_meandaily$doy,yough_meandaily$mean+yough_meandaily$se, length=.025, angle=90)
-arrows(yough_meandaily$doy,yough_meandaily$mean, yough_meandaily$doy,yough_meandaily$mean-yough_meandaily$se, length=.025, angle=90)
+plot(FlowData_meandaily$mean, type="l", col="blue")
+arrows(FlowData_meandaily$doy,FlowData_meandaily$mean, FlowData_meandaily$doy,FlowData_meandaily$mean+FlowData_meandaily$se, length=.025, angle=90)
+arrows(FlowData_meandaily$doy,FlowData_meandaily$mean, FlowData_meandaily$doy,FlowData_meandaily$mean-FlowData_meandaily$se, length=.025, angle=90)
 
-df <- yough
+df <- FlowData
 
-yough_meandaily$datenew <- as.Date(yough_meandaily$doy, origin = "2016-01-01")
-yough_meandaily$month <- months(yough_meandaily$date)
+FlowData_meandaily$datenew <- as.Date(FlowData_meandaily$doy, origin = "2016-01-01")
+FlowData_meandaily$month <- months(FlowData_meandaily$date)
 
-#yough_meandaily$period <- if(yough_meandaily$)
+#FlowData_meandaily$period <- if(FlowData_meandaily$)
 
 library(tidyverse)
-ggplot(yough_meandaily, aes(x=datenew, y=mean, group=period)) + # 
+ggplot(FlowData_meandaily, aes(x=datenew, y=mean, group=period)) + # 
   geom_line(size=1, aes(color=period)) + #shape = 21, 
   geom_ribbon(aes(ymin=mean-sd, ymax=mean+sd,fill=period),  alpha=0.2) +
   scale_x_date(date_breaks = "1 month", date_labels = "%B") +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
-  labs(title = "mean daily flow - USGS 03081500 Youghiogheny River at Ohiopyle, PA",
+  labs(title = "mean daily flow - USGS 03081500 FlowDataiogheny River at Ohiopyle, PA",
      y="Mean Daily Flow (cfs)",
      x="Date") + theme_bw(base_size = 8) +
-  coord_cartesian(ylim = c(0, max(yough_meandaily$mean))) 
+  coord_cartesian(ylim = c(0, max(FlowData_meandaily$mean))) 
 
 
 
